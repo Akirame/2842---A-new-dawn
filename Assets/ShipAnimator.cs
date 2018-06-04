3879e2a89d78e860bf9924f1116ad5e4896cdef0
Akirame <gsvillalba12@hotmail.com>
2018-06-04 18:52:25 -0300

    Initial commit

---
+++ Assets/ShipAnimator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipAnimator : MonoBehaviour
+{
+    private Animator animator;
+    // Use this for initialization
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+        animator.SetFloat("dir",horizontal);
+    }
+}
