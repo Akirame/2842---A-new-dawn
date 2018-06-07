c31ac2bfccead73d5d5334bb2a383f4c379d53ed
Akirame <gsvillalba12@hotmail.com>
2018-06-06 21:09:30 -0300

    Revert "Revert "Add Camera Singleton""
    This reverts commit cc4e4f718e8f546a77ecb918f65a974899541e57.

--- Assets/00_Game/Scripts/Bullet.cs
+++ Assets/00_Game/Scripts/Bullet.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed; 
+    public float speed;
+    public Camera cam;
 
     private void Update()
     {
+        OffCamera();
         transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+                
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OffCamera()
     {
-        if (collision.gameObject.tag == "BoundWall")
+        Vector3 PosInCamera = cam.WorldToViewportPoint(transform.position);
+        if (PosInCamera.y > 1 || PosInCamera.x < 0 || PosInCamera.x > 1)
+        {
             Destroy(this.gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
     }
 }
