f70c0d745cc9e903846617c9c0db6c881315bee7
Akirame <gsvillalba12@hotmail.com>
2018-06-12 18:34:23 -0300

    Add credits

--- Assets/00_Game/Scripts/UI/UIFinalScreen.cs
+++ Assets/00_Game/Scripts/UI/UIFinalScreen.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIFinalScreen : MonoBehaviour {
-
+public class UIFinalScreen : MonoBehaviour
+{
     public Text scoreText;
 
     private void Start()
     {
         scoreText.text = "Score : " + GameManager.Get().GetScore();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+        }
     }
 }
